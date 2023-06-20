53c4b0cca0d59285f1895b029009ad22e694f4c2
k6mil6 <kamil_6@vk.com>
2023-06-21 00:02:20 +0300

    ---

--- eTickets/Controllers/CinemasController.cs
+++ eTickets/Controllers/CinemasController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace eTickets.Controllers
 {
     public class CinemasController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+
+        public CinemasController(AppDbContext context)
         {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allCinemas = await _context.Cinemas.ToListAsync();
             return View();
         }
     }
--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -7,9 +7,14 @@ namespace eTickets.Models
         [Key]
         public int Id { get; set; }
 
+        [Display(Name = "Profile picture URL")]
         public string ProfilePictureURL { get; set; }
-        public string FullName { get; set;}
-        public string Bio { get; set; }
+
+		[Display(Name = "Full Name")]
+		public string FullName { get; set;}
+
+		[Display(Name = "Biography")]
+		public string Bio { get; set; }
 
 
         //Relationship
